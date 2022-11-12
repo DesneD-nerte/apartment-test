96d97a444b84fc617e331b91420e860e8a7dfc97
Alexandr Ershov <ershov.avdd@gmail.com>
2022-11-12 15:13:40 +0700

    Added webpack option, env

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { AppRouter } from "./router/AppRouter";
+import {
+    useQuery,
+    useMutation,
+    useQueryClient,
+    QueryClient,
+    QueryClientProvider,
+} from "@tanstack/react-query";
 import "./styles/styles.css";
 
+const queryClient = new QueryClient();
+
 const App = () => {
-    return <RouterProvider router={AppRouter} />;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={AppRouter} />;
+        </QueryClientProvider>
+    );
 };
 
 export default App;
--- client/src/components/apartment/apartment-list/ApartmentList.tsx
+++ client/src/components/apartment/apartment-list/ApartmentList.tsx
@@ -1,13 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
-import ApartmentItem from "../apartment-item/apartmentItem";
+import ApartmentItem from "../apartment-item/ApartmentItem";
 
 const fetchApartments = async () => {
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     return res.json();
 };
 
+const backendApi = process.env.BACKEND_API_URL;
+
 const ApartmentList = () => {
-    const [apartmentData, setApartmentData] = useState();
+    // const [apartmentData, setApartmentData] = useState();
+    const { isLoading, error, data } = useQuery({
+        queryKey: ["apartmentData"],
+        queryFn: () => {
+            fetch(`${backendApi}/apartments`).then((res) => res.json());
+        },
+    });
+
+    if (isLoading) return "Loading...";
+
+    if (error) return "An error has occurred: " + error;
+
     return (
         <div>
             <ApartmentItem></ApartmentItem>
